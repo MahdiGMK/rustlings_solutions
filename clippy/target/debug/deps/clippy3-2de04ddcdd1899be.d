0df3d656903f515a1991622d36e240d54510501f
MahdiGMK <mbahramiand@gmail.com>
2024-02-13 21:10:58 +0330

    initial commit

---
+++ clippy/target/debug/deps/clippy3-2de04ddcdd1899be.d
@@ -0,0 +1,9 @@
+/home/mahdigmk/DevEnv/rustLearning/rustlings/exercises/clippy/target/debug/deps/libclippy3-2de04ddcdd1899be.rmeta: clippy3.rs Cargo.toml
+
+/home/mahdigmk/DevEnv/rustLearning/rustlings/exercises/clippy/target/debug/deps/clippy3-2de04ddcdd1899be.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
---
+++ quiz3.rs
@@ -0,0 +1,79 @@
+// quiz3.rs
+//
+// This quiz tests:
+// - Generics
+// - Traits
+//
+// An imaginary magical school has a new report card generation system written
+// in Rust! Currently the system only supports creating report cards where the
+// student's grade is represented numerically (e.g. 1.0 -> 5.5). However, the
+// school also issues alphabetical grades (A+ -> F-) and needs to be able to
+// print both types of report card!
+//
+// Make the necessary code changes in the struct ReportCard and the impl block
+// to support alphabetical report cards. Change the Grade in the second test to
+// "A+" to show that your changes allow alphabetical grades.
+//
+// Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
+
+use std::fmt::Display;
+
+enum Grade {
+    Num(f32),
+    Alph(String),
+}
+impl Display for Grade {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Grade::Num(a) => f.write_str(a.to_string().as_str()),
+            Grade::Alph(a) => f.write_str(a),
+        }
+    }
+}
+
+pub struct ReportCard {
+    pub grade: Grade,
+    pub student_name: String,
+    pub student_age: u8,
+}
+
+impl ReportCard {
+    pub fn print(&self) -> String {
+        format!(
+            "{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, &self.grade
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn generate_numeric_report_card() {
+        let report_card = ReportCard {
+            grade: Grade::Num(2.1),
+            student_name: "Tom Wriggle".to_string(),
+            student_age: 12,
+        };
+        assert_eq!(
+            report_card.print(),
+            "Tom Wriggle (12) - achieved a grade of 2.1"
+        );
+    }
+
+    #[test]
+    fn generate_alphabetic_report_card() {
+        // TODO: Make sure to change the grade here after you finish the exercise.
+        let report_card = ReportCard {
+            grade: Grade::Alph("A+".into()),
+            student_name: "Gary Plotter".to_string(),
+            student_age: 11,
+        };
+        assert_eq!(
+            report_card.print(),
+            "Gary Plotter (11) - achieved a grade of A+"
+        );
+    }
+}
